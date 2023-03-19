cf341412790c70e11cc03e19fcbc7aa9490225aa
RomaKlyukin <125215143+RomaKlyukin@users.noreply.github.com>
2023-03-19 11:28:49 +0300

    b108

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int s = 0;
+  int sc = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        s++;
+        sc++;
       }
     }
   }
-  return s;
+  return sc;
 }
 int countPairs2(int *arr, int len, int value) {
   int sc = 0;
